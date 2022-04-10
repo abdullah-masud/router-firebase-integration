0fae878ce7eaa05f5bcfd713c4679f9bde7f45e3
Abdullah Al Masud <masudctg00@gmail.com>
2022-04-11 01:14:43 +0600

    moving on to different auth hooks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Products from './components/Products/Products';
 import Register from './components/Register/Register';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<Login />}></Route>
         <Route path='/register' element={<Register />}></Route>
+        <Route path='/products' element={<Products />}></Route>
       </Routes>
     </div>
   );
